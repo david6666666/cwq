27b35be2d71162377992faf88f4ff1eb17f2516f
cwq <530634352@qq.com>
2019-09-14 09:49:14 +0800

    1

--- 123.py
+++ 123.py
@@ -1,1 +1,11 @@
+def maxLoss(prices):
+    r = 0
+    for i in range(1, len(prices)):
+        v = prices[i] - prices[i - 1]
+        r += v if v < 0 else 0
+    return r
 
+
+
+arr = list(map(int,input().split(',')))
+print(abs(maxLoss(arr)))
